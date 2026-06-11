9651851d565585caa627f0be13f939f745f1d53d
jorgeandrecastro <georgeandrec@gmail.com>
2026-06-11 22:59:00 +0200

    Doc ok

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-only
-// Copyright (C) 2025 Andre
+// Copyright (C) 2025 Jorge Andre Castro
 //
 // This program is free software; you can redistribute it and/or modify it
 // under the terms of the GNU General Public License version 2 as published
